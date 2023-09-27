2f2ad44bd9f42fb2c40fa6a934138f61cada0a66
kaparin <pompario@gmail.com>
2023-09-27 15:38:12 +0300

    С нами Бог

--- keystone.ts
+++ keystone.ts
@@ -13,6 +13,7 @@ import { lists } from './schema';
 // authentication is configured separately here too, but you might move this elsewhere
 // when you write your list-level access control functions, as they typically rely on session data
 import { withAuth, session } from './auth';
+import { isSignedIn } from '@keystone-next/keystone/access';
 
 export default withAuth(
   config({
